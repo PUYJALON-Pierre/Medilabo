e105a8e496de92b77985ace0a44c588b2ff00380
PUYJALON Pierre <puyjalon.pierre@gmail.com>
2023-10-06 10:25:15 +0200

    Creation of gateway-server (working port:9103)
    -Adding dependencies : Eureka Client, Cloud Gateway, Spring Security,
    Actuator, Cloud Config)
    
    -Creating a route for patient-ms
    
    -Externalize gateway properties into distant repository
    
    -Use of SpringWebflux instead of SpringWeb in patient-ms in order to
    handle no queue pattern of Spring cloud Gateway

---
+++ gateway-server/src/main/java/com/medilabo/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,39 @@
+package com.medilabo.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	
+	        return builder.routes()
+	                .route("patient-ms", r -> r.path("/patient/**")
+	                        .uri("lb://PATIENT-MS"))
+	                
+	                .build();   
+	}
+	
+	
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+	return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+	}
+	
+	
+}
