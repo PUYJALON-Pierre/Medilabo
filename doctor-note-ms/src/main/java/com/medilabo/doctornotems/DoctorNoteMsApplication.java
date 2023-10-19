6432aaa40b30edd635cf88efa13160240d99bfe1
PUYJALON Pierre <puyjalon.pierre@gmail.com>
2023-10-19 16:23:16 +0200

    Creation of project structure for doctor-note-ms + properties
    -Adding dependencies
    -Creation of packages
    -Externalize properties on distant repository with SpringConfig
    -Setting properties in order to connect to Mongo database

---
+++ doctor-note-ms/src/main/java/com/medilabo/doctornotems/DoctorNoteMsApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.doctornotems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorNoteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorNoteMsApplication.class, args);
+	}
+
+}
---
+++ doctor-note-ms/src/test/java/com/medilabo/doctornotems/DoctorNoteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.doctornotems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorNoteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
