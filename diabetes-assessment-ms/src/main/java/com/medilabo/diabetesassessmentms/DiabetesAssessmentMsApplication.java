0b40106cdcb40d37354fc21bf614e3ca9ad1f938
PUYJALON Pierre <puyjalon.pierre@gmail.com>
2023-10-27 22:30:27 +0200

    Creating project structure of diabetes-assessment-ms
    -Adding dependencies and creating packages
    -Externalize properties to distant repository

---
+++ diabetes-assessment-ms/src/main/java/com/medilabo/diabetesassessmentms/DiabetesAssessmentMsApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.diabetesassessmentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesAssessmentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesAssessmentMsApplication.class, args);
+	}
+
+}
---
+++ diabetes-assessment-ms/src/test/java/com/medilabo/diabetesassessmentms/DiabetesAssessmentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.diabetesassessmentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesAssessmentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
